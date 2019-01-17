db837e26df715a0f50b3f75cabee54c9c3d75950
litinsky <alitinsky@gmail.com>
2019-01-17 12:18:02 +0300

    Implemented Geolight wrapper

--- model/log_tools.py
+++ model/log_tools.py
@@ -1,8 +1,7 @@
 import hashlib
 import gzip
-from typing import Optional
 from pathlib import Path
-from datetime import date, timedelta
+from datetime import date
 
 import requests
 from maxminddb import open_database
@@ -11,8 +10,8 @@ from maxminddb.reader import Reader
 
 GEOLITE_DB_ARC_URL = 'http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz'
 GEOLITE_HASH_URL = 'http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.md5'
-GEOLITE_DB_PATH = './temp/GeoLite2-City.mmdb'
-GEOLITE_HASH_PATH = './temp/GeoLite2-City.mmdb.md5'
+GEOLITE_DB_PATH = '../temp/GeoLite2-City.mmdb'
+GEOLITE_HASH_PATH = '../temp/GeoLite2-City.mmdb.md5'
 
 
 def get_file_md5_hash(file_path: Path) -> str:
@@ -27,21 +26,7 @@ def get_file_md5_hash(file_path: Path) -> str:
     return file_hash
 
 
-"""
-def _update_geolite_db():
-    dp_arc_url = GEOLITE_DB_ARC_URL
-    hash_url = GEOLITE_HASH_URL
-
-    arc_path = Path(GEOLITE_ARC_PATH).resolve()
-    db_path = Path(GEOLITE_DB_PATH).resolve()
-
-    if db_path.is_file():
-        last_mod_date = date.fromtimestamp(db_path.stat().st_mtime)
-        if True: #date.today() > last_mod_date:
-"""
-
-
-class _GeoliteDb:
+class _GeoliteDbWrapper:
     def __init__(self) -> None:
         self._dp_arc_url = GEOLITE_DB_ARC_URL
         self._hash_url = GEOLITE_HASH_URL
@@ -54,9 +39,7 @@ class _GeoliteDb:
     def _update_db(self) -> bool:
         updated = False
 
-        if True: #date.today() > self._db_upd_check_date:
-            print('Performing update check...')
-
+        if date.today() > self._db_upd_check_date:
             download_hash = requests.get(self._hash_url).text
 
             if self._hash_path.is_file():
@@ -66,8 +49,6 @@ class _GeoliteDb:
                 current_hash = ''
 
             if not self._db_path.is_file() or download_hash != current_hash:
-                print('Updating...')
-
                 self._db_path.parent.mkdir(exist_ok=True, parents=True)
                 dp_arc_request = requests.get(url=self._dp_arc_url)
 
@@ -88,22 +69,3 @@ class _GeoliteDb:
         self._update_db()
         result = self._db_reader.get(ip_address)
         return result
-
-
-if __name__ == '__main__':
-    db = _GeoliteDb()
-    print(db.get_ip_info('196.196.221.36'))
-
-    #db_path = Path(GEOLITE_DB_PATH)
-    #db_path.parent.mkdir(exist_ok=True, parents=True)
-    #dp_arc_request = requests.get(url=GEOLITE_DB_ARC_URL)
-    #
-    #with db_path.open('wb') as f_db:
-    #    f_db.write(gzip.decompress(dp_arc_request.content))
-
-    #dt = date.today()
-    #tdt = timedelta(days=1)
-    #print(date.fromtimestamp(0))
-    #print(dt)
-    #print(dt + tdt)
-    #print(requests.get(GEOLITE_HASH_URL).text)
