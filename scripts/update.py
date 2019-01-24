70a19e758893dadb5c3660814a50106e8c282f73
litinsky <alitinsky@gmail.com>
2019-01-24 17:13:29 +0300

    Added processed log dataframe update to update script

--- scripts/update.py
+++ scripts/update.py
@@ -2,7 +2,7 @@ import logging
 from pathlib import Path
 
 from log_analyser.log_dataframe import LogDataFrame
-from log_analyser.log_configs import RAW_FILES_LOG_CONFIG
+from log_analyser.log_configs import RAW_FILES_LOG_CONFIG, PROCESSED_FILES_LOG_CONFIG
 
 
 def update() -> None:
@@ -18,13 +18,18 @@ def update() -> None:
 
     logger.addHandler(file_handler)
 
-    logger.info('Started updating raw logs Dataframe...')
+    logger.info('Started updating logs dataframes...')
 
     raw_ldf = LogDataFrame(RAW_FILES_LOG_CONFIG)
-    df_upd = raw_ldf.update()
-    df_upd_rows_count = 0 if df_upd is None else df_upd.shape[0]
+    raw_ldf_upd = raw_ldf.update()
+    raw_ldf_upd_rows_count = 0 if raw_ldf_upd is None else raw_ldf_upd.shape[0]
 
-    logger.info(f'Finished updating raw logs Dataframe with {df_upd_rows_count} rows')
+    log_ldf = LogDataFrame(PROCESSED_FILES_LOG_CONFIG)
+    log_ldf_upd = log_ldf.update(raw_ldf.df())
+    log_ldf_upd_rows_count = 0 if log_ldf_upd is None else log_ldf_upd.shape[0]
+
+    logger.info(f'Finished updating raw logs dataframe with {raw_ldf_upd_rows_count} rows '
+                f'and log dataframe with {log_ldf_upd_rows_count} rows')
 
 
 if __name__ == '__main__':
