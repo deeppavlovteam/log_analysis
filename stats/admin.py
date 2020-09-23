9b389510615bc4df98a1cc922962c2e24e9d8e94
ignatov <ignatov.fedor@gmail.com>
2020-09-23 16:56:37 +0300

    feat: custom admin view

--- stats/admin.py
+++ stats/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Record
+
+
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ('ip', 'file', 'config', 'outer_request')
+#    fields = ['file', 'time']
+    list_filter = ['outer_request']
+#    search_fields = ['ip', 'config']
+
+admin.site.register(Record, RecordAdmin)
