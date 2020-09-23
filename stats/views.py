5e210f9b08c44ebb586017135f97aec2f7dd1de9
ignatov <ignatov.fedor@gmail.com>
2020-09-23 14:26:59 +0300

    feat: added views

--- stats/views.py
+++ stats/views.py
@@ -1,5 +1,18 @@
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
+
+from .models import Record
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the stats index.")
+    latest_records = Record.objects.order_by('-time')[:5]
+    context = {'latest_records': latest_records}
+    return render(request, 'stats/index.html', context)
+
+
+def detail(request, id):
+    try:
+        record = Record.objects.get(id=id)
+    except Record.DoesNotExist:
+        raise Http404("Record does not exist")
+    return render(request, 'stats/detail.html', {'record': record})
