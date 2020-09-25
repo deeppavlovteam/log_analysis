b64cee1bf1aed4a263a7e1a3b4fa2967f812c703
ignatov <ignatov.fedor@gmail.com>
2020-09-25 10:02:26 +0300

    feat: request time range filter

--- deepstats/settings.py
+++ deepstats/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rangefilter',
     'django_filters',
     'stats.apps.StatsConfig',
     'django.contrib.admin',
--- stats/admin.py
+++ stats/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from .models import Record, Hash, File, Config
 from django.db.models import Count, Q
 from django.contrib.admin import SimpleListFilter
+from rangefilter.filter import DateRangeFilter, DateTimeRangeFilter
 
 
 class ResponseCodeFilter(SimpleListFilter):
@@ -31,6 +32,14 @@ class OuterRequestFilter(SimpleListFilter):
         return queryset
 
 
+class MyDateFilter(DateRangeFilter):
+    def __init__(self, field, request, params, model, model_admin, field_path):
+        super(MyDateFilter, self).__init__('asdf', request, params, model, model_admin, 'time')
+
+    def queryset(self, request, queryset):
+        return queryset
+
+
 class RecordAdmin(admin.ModelAdmin):
     list_display = ('ip', 'file', 'config', 'outer_request')
 #    fields = ['file', 'time']
@@ -42,18 +51,30 @@ class RecordAdmin(admin.ModelAdmin):
 class FileAdmin(admin.ModelAdmin):
     list_display = ('name', 'n_records')
     search_fields = ['name']
-    list_filter = (ResponseCodeFilter, OuterRequestFilter, 'md5')
+    list_filter = (ResponseCodeFilter, OuterRequestFilter, 'md5', ('name', MyDateFilter))
 
     def get_queryset(self, request):
         qs = super(admin.ModelAdmin, self).get_queryset(request)
 
         default_filter = Q()
         for filter in self.list_filter:
+            print(request.GET)
             if isinstance(filter, str):
                 continue
-            val = request.GET.get(filter.parameter_name)
-            if val is not None:
-                default_filter &= Q(**{f'record__{filter.parameter_name}': val})
+            elif isinstance(filter, tuple):
+                gte = request.GET.get('time__range__gte')
+                lte = request.GET.get('time__range__lte')
+                dic = {}
+                if gte is not None:
+                    dic.update({'record__time__gte': gte})
+                if lte is not None:
+                    dic.update({'record__time__lte': lte})
+                if dic:
+                    default_filter &= Q(**dic)
+            else:
+                val = request.GET.get(filter.parameter_name)
+                if val is not None:
+                    default_filter &= Q(**{f'record__{filter.parameter_name}': val})
 
         return qs.annotate(n_records=Count('record', filter=default_filter))
 
